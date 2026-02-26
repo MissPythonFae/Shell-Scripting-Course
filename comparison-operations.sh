7d37f02d24ca2d7afa486770e2614f513439bf2a
faeze <faezem412@gmail.com>
2026-02-26 15:05:15 +0330

    comparison

---
+++ comparison-operations.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+
+# intiger : -eq(=), -ne(!=), -gt(>), -ge(>=), -lt(<), -le(=<), ==, =(whit distanc)
+# string : ==, =, !=, -z(emty), -n(not empty), ! z(not empty)
+
+n1="faeze"
+
+if [ -z $n1 ]
+then 
+	echo "done..."
+fi
+
